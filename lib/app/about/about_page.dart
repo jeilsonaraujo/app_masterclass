1fee188e0ef61e9c91e532ff65d32eccca9158c7
Decripter <jeilsonaraujo@icloud.com>
2022-09-10 10:24:22 -0300

    feat: adding header tho this page

--- lib/app/about/about_page.dart
+++ lib/app/about/about_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:app_masterclass/components/header_page_widget.dart';
+
 class AboutPage extends StatefulWidget {
   const AboutPage({Key? key}) : super(key: key);
 
@@ -10,6 +12,8 @@ class AboutPage extends StatefulWidget {
 class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: HeaderPageWidget(title: 'Sobre o dev'),
+    );
   }
 }
