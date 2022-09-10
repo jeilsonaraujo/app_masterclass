5b4a9962666b379fe744f730d48f0b770e8c1515
Decripter <jeilsonaraujo@icloud.com>
2022-09-10 15:20:12 -0300

    fix: fix issue on skill experience level

--- lib/app/about/components/skill_item_widget.dart
+++ lib/app/about/components/skill_item_widget.dart
@@ -3,39 +3,41 @@ import 'package:flutter/material.dart';
 class SkillItemWidget extends StatelessWidget {
   final String title;
   final double experienceLevel;
+
+  ///The [experienceLevel] shoud between 0.0 and 1.0
   const SkillItemWidget({
     super.key,
     required this.title,
+
+    ///Level of hability on this skill value between 0.0 to 1.0
     required this.experienceLevel,
   });
 
   @override
   Widget build(BuildContext context) {
+    final double widthFiftyPercent = MediaQuery.of(context).size.width * 0.5;
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Row(
         children: [
           Expanded(flex: 1, child: Text(title)),
-          Expanded(
-            flex: 2,
-            child: Container(
-              height: 9,
-              decoration: BoxDecoration(
-                color: Theme.of(context).canvasColor,
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(25),
-                ),
+          Container(
+            height: 9,
+            width: widthFiftyPercent,
+            decoration: BoxDecoration(
+              color: Theme.of(context).canvasColor,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(25),
               ),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Container(
-                  height: 9,
-                  width: experienceLevel * 20,
-                  decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                      borderRadius:
-                          const BorderRadius.all(Radius.circular(25))),
-                ),
+            ),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                height: 9,
+                width: widthFiftyPercent * experienceLevel,
+                decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: const BorderRadius.all(Radius.circular(25))),
               ),
             ),
           )
