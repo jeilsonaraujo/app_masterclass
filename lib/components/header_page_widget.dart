6306f10a47341912f7a109a567553656884cf1bd
Decripter <jeilsonaraujo@icloud.com>
2022-09-10 10:09:19 -0300

    feat: adding a global component, a header to reuse on all screens

---
+++ lib/components/header_page_widget.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'package:app_masterclass/app/theme_controller.dart';
+
+class HeaderPageWidget extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  const HeaderPageWidget({super.key, required this.title});
+  final double height = 120.0;
+  @override
+  Widget build(BuildContext context) {
+    final controller =
+        context.dependOnInheritedWidgetOfExactType<ThemeController>()!;
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 15, right: 15),
+          child: SizedBox(
+            height: 92,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 4.0),
+                      child: Image.asset(
+                        'assets/logo.png',
+                        height: 44,
+                        width: 44,
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          title,
+                          style: const TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        const Text(
+                          'Fluterando Masterclass',
+                          style: TextStyle(
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                IconButton(
+                  onPressed: () {
+                    controller.toggle();
+                  },
+                  icon: const Icon(FontAwesomeIcons.solidMoon, size: 28),
+                )
+              ],
+            ),
+          ),
+        ),
+        SizedBox(height: 27)
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(height);
+}
