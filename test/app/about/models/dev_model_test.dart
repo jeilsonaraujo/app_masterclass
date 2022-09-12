e6fd35afa10b2f05d8847495091f59be76a37bac
Decripter <jeilsonaraujo@icloud.com>
2022-09-12 14:56:51 -0300

    feat: add bloc to dev skills

--- test/app/about/models/dev_model_test.dart
+++ test/app/about/models/dev_model_test.dart
@@ -1,4 +1,5 @@
 import 'package:app_masterclass/app/about/model/dev_model.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -8,21 +9,12 @@ void main() {
     "node_id": "MDQ6VXNlcjQ2NjQ5NTMx",
     "avatar_url": "https://avatars.githubusercontent.com/u/46649531?v=4",
     "name": "Jeilson Araujo",
-    "company": null,
-    "blog": "",
-    "location": "Brazil",
-    "email": null,
-    "hireable": null,
     "bio": "Always learning...\r\nCurrently Dart/Flutter",
     "twitter_username": null,
     "public_repos": 35,
     "public_gists": 0,
     "followers": 3,
-    "following": 2,
-    "created_at": "2019-01-13T14:05:58Z",
-    "updated_at": "2022-09-11T15:09:25Z"
   };
-
   test('shoud return user name [Jeilson Araujo]', () {
     var dev = DevModel.fromMap(data);
     expect(dev.name, 'Jeilson Araujo');
@@ -37,5 +29,12 @@ void main() {
     var dev = DevModel.fromMap(data);
     expect(
         dev.avatar_url, 'https://avatars.githubusercontent.com/u/46649531?v=4');
+  });
+
+  test("shoud return data from github", () async {
+    var dio = Dio();
+    final response = await dio.get('https://api.github.com/users/decripter');
+    var dev = DevModel.fromMap(response.data);
+    expect(dev.name, 'Jeilson Araujo');
   });
 }
