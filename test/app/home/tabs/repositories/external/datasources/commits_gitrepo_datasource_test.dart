34229d2da9aae8d1c95fb8dbb1d41ca5cd1e30b0
Decripter <jeilsonaraujo@icloud.com>
2022-10-01 16:44:42 -0300

    test: add tests for external/datasource

---
+++ test/app/home/tabs/repositories/external/datasources/commits_gitrepo_datasource_test.dart
@@ -0,0 +1,18 @@
+import 'package:uno/uno.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'commits_gitrepo_datasource_test.mocks.dart';
+
+import 'package:app_masterclass/app/home/tabs/repositories/external/datasources/commits_gitrepo_datasource.dart';
+
+@GenerateNiceMocks([MockSpec<Uno>()])
+final unoMock = MockUno();
+final commitsDatasource = CommitsGitRepoDatasource(uno: unoMock);
+
+void main() {
+  test('Should return a list of data', () async {
+    when(unoMock.get('')).thenReturn(new Future.value());
+  });
+}
