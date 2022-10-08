c23bf36918cfd3ab6b6d8cafbfe1e1d658e17352
Decripter <jeilsonaraujo@icloud.com>
2022-10-08 00:07:08 -0300

    refact: remove componensts no longer necessary

--- lib/app/home/tabs/about/dependencies/skills_injection.dart
+++
@@ -1,6 +0,0 @@
-import 'package:app_masterclass/app/home/tabs/about/blocs/bloc_skills/skills_bloc.dart';
-
-class SkillsInjection {
-  final SkillsBloc skillsBloc;
-  SkillsInjection({required this.skillsBloc});
-}
